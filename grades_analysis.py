b36687d6e338a314459d2fdf821e07e94083784c
vladhrycheniuk <vladgrycheniukwork@gmail.com>
2025-08-18 11:43:44 +0200

    add grades analysis script

---
+++ grades_analysis.py
@@ -0,0 +1,17 @@
+grades = [67, 89, 45, 100, 76, 88, 92, 58]
+
+# Середнє значення
+average = sum(grades) / len(grades)
+
+# Мінімум і максимум
+min_grade = min(grades)
+max_grade = max(grades)
+
+# Сортування
+sorted_grades = sorted(grades)
+
+print("Оцінки:", grades)
+print("Середнє значення:", average)
+print("Мінімальна оцінка:", min_grade)
+print("Максимальна оцінка:", max_grade)
+print("Відсортовані оцінки:", sorted_grades)
